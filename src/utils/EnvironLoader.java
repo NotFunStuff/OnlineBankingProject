9e6aaf0887cfdc2b48997adabc8b7412e92d9b08
Long <hoanglongmit@gmail.com>
2021-01-25 01:38:40 +0700

    env loader v1

---
+++ src/utils/EnvironLoader.java
@@ -0,0 +1,47 @@
+package utils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+/**
+ *
+ * @author long
+ */
+public class EnvironLoader {
+    /* HOW TO USE:
+        // put env vars in /src/assets/env.txt
+        var env = new EnvironLoader();
+        System.out.println(env.get("password"));
+    */
+
+    final HashMap envDict = new HashMap<String, String>();
+
+    public EnvironLoader() {
+        try {
+            File myObj = new File("./src/assets/env.txt");
+            try ( Scanner myReader = new Scanner(myObj)) {
+                while (myReader.hasNextLine()) {
+                    String line = myReader.nextLine();
+                    int eqlInd = line.indexOf("=");
+                    if (eqlInd != -1) {
+                        String key = line.substring(0, eqlInd);
+                        key = key.toLowerCase().trim();
+                        String value = line.substring(eqlInd+1, line.length());
+                        value = value.toLowerCase().trim();
+
+                        this.envDict.put(key, value);
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("NO env.txt FOUND!!!");
+        }
+    }
+
+    public String get(String key) {
+        return (String) this.envDict.get(key.toLowerCase().trim());
+    }
+
+}
