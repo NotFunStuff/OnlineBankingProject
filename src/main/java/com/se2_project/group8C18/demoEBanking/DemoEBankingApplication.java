0ddbb1ec28c3d50d49370b6868c9f342ffa7fad9
concua0z <61311658+concua0z@users.noreply.github.com>
2021-02-27 09:24:35 +0700

    fix authentication

--- src/main/java/com/se2_project/group8C18/demoEBanking/DemoEBankingApplication.java
+++ src/main/java/com/se2_project/group8C18/demoEBanking/DemoEBankingApplication.java
@@ -2,12 +2,14 @@ package com.se2_project.group8C18.demoEBanking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
-public class DemoEBankingApplication {
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class DemoEBankingApplication extends ServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoEBankingApplication.class, args);
 	}
 
 }
+	
